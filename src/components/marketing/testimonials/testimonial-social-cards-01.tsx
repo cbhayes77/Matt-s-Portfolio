cb294844db329d5449a7c73fecb60cd82881504c
“cbhayes77” <chris@chrishayes.me>
2026-04-17 00:50:40 -0500

    Updated testimonials

--- src/components/marketing/testimonials/testimonial-social-cards-01.tsx
+++ src/components/marketing/testimonials/testimonial-social-cards-01.tsx
@@ -1,63 +1,28 @@
-import { AvatarLabelGroup } from "@/components/base/avatar/avatar-label-group";
-import { VerifiedTick } from "@/components/base/avatar/base-components";
 
 const reviews = [
     {
         id: "review-01",
-        quote: "We've been using Untitled to kick start every new project and can't imagine working without it.",
-        author: {
-            name: "Sienna Hewitt",
-            title: "Project Manager, Warpspeed",
-            imageUrl: "https://www.untitledui.com/images/avatars/sienna-hewitt?fm=webp&q=80",
-        },
-        company: {
-            name: "Warpspeed",
-            imageUrl: "https://www.untitledui.com/logos/logotype/color/warpspeed.svg",
-            imageUrlDark: "https://www.untitledui.com/logos/logotype/white/warpspeed.svg",
-        },
+        quote: "Matt partnered with my scrum team to design, develop, and maintain learning experiences that increased customer ease, quality, and efficiency, and contributed to measurable business results.",
     },
     {
         id: "review-02",
-        quote: "Untitled has become an essential part of our design process. It speeds up our workflow and ensures every project starts with a solid foundation.",
-        author: {
-            name: "Caitlyn King",
-            title: "COO, OdeaoLabs",
-            imageUrl: "https://www.untitledui.com/images/avatars/caitlyn-king?fm=webp&q=80",
-        },
-        company: {
-            name: "Warpspeed",
-            imageUrl: "https://www.untitledui.com/logos/logotype/color/odeao-labs.svg",
-            imageUrlDark: "https://www.untitledui.com/logos/logotype/white/odeao-labs.svg",
-        },
+        quote: "Matt has deep technical experience in our processes, tools, and design work. He leads the creation of tech-enabled learning experiences that are personalized, clear, and effective.",
     },
     {
         id: "review-03",
-        quote: "Every project starts with Untitled, and it's made a huge difference in our output. It's a game-changer for our design team.",
-        author: {
-            name: "Olly Schroeder",
-            title: "Designer, Nietzsche",
-            imageUrl: "https://www.untitledui.com/images/avatars/olly-schroeder?fm=webp&q=80",
-            imageUrlDark: "https://www.untitledui.com/logos/logotype/white/nietzsche.svg",
-        },
-        company: {
-            name: "Warpspeed",
-            imageUrl: "https://www.untitledui.com/logos/logotype/color/nietzsche.svg",
-            imageUrlDark: "https://www.untitledui.com/logos/logotype/white/nietzsche.svg",
-        },
+        quote: "Matt has a proven ability to work in close conjunction with stakeholders, business owners, SMEs, and peers, increasing buy-in and confidence while helping projects move faster.",
     },
     {
         id: "review-04",
-        quote: "Using Untitled has streamlined our entire design process. It's an invaluable part of our studio!",
-        author: {
-            name: "Riley O'Moore",
-            title: "Design Engineer, QuartzAI",
-            imageUrl: "https://www.untitledui.com/images/avatars/riley-moore?fm=webp&q=80",
-        },
-        company: {
-            name: "QuartzAI",
-            imageUrl: "https://www.untitledui.com/logos/logotype/color/quartz-ai.svg",
-            imageUrlDark: "https://www.untitledui.com/logos/logotype/white/quartz-ai.svg",
-        },
+        quote: "Matt delivers the right outcomes and makes effective decisions daily. He prioritizes the right tasks, ensures deadlines are met, and is a reliable, supportive teammate.",
+    },
+    {
+        id: "review-05",
+        quote: "Matt is adept at stepping into work already in progress and contributes significantly to the team's ability to respond nimbly to aggressive and uncertain timelines.",
+    },
+    {
+        id: "review-06",
+        quote: "Matt takes and implements performance feedback well. He is receptive to coaching and a pleasure to problem-solve with.",
     },
 ];
 
@@ -72,29 +37,10 @@ export const TestimonialSocialCards01 = () => {
                 {reviews.map((review) => (
                     <div
                         key={review.id}
-                        className="flex flex-col gap-12 rounded-xl bg-primary_alt p-6 shadow-xs ring-1 ring-secondary lg:min-h-64 lg:justify-between lg:gap-0 lg:p-8"
+                        className="flex flex-col gap-12 rounded-xl bg-primary_alt p-6 shadow-xs ring-1 ring-secondary lg:min-h-32 lg:justify-between lg:gap-0 lg:p-8"
                     >
                         <div className="flex flex-col items-start gap-3">
-                            <img className="h-8 lg:hidden dark:hidden" src={review.company.imageUrl} alt={review.company.name} />
-                            <img className="h-8 opacity-85 not-dark:hidden lg:hidden" src={review.company.imageUrlDark} alt={review.company.name} />
-                            <blockquote className="text-lg font-medium text-primary lg:text-xl">{review.quote}</blockquote>
-                        </div>
-                        <div className="flex justify-between">
-                            <AvatarLabelGroup
-                                border
-                                size="lg"
-                                src={review.author.imageUrl}
-                                alt={review.author.name}
-                                title={
-                                    <span className="relative flex items-center gap-1">
-                                        {review.author.name}
-                                        <VerifiedTick size="lg" />
-                                    </span>
-                                }
-                                subtitle={review.author.title}
-                            />
-                            <img className="h-8 max-lg:hidden dark:hidden" src={review.company.imageUrl} alt={review.company.name} />
-                            <img className="h-8 opacity-85 not-dark:hidden max-lg:hidden" src={review.company.imageUrlDark} alt={review.company.name} />
+                            <blockquote className="text-lg font-medium text-primary lg:text-xl">&ldquo;{review.quote}&rdquo;</blockquote>
                         </div>
                     </div>
                 ))}
